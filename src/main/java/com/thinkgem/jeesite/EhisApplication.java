9c521ec15e0829fd754fea202c40ce25c18d15b2
bian1234 <ykbian@qq.com>
2018-08-27 14:48:33 +0800

    基本能启动
    还存在一个问题。NoClassDefFoundError

--- src/main/java/com/thinkgem/jeesite/EhisApplication.java
+++ src/main/java/com/thinkgem/jeesite/EhisApplication.java
@@ -3,10 +3,13 @@ package com.thinkgem.jeesite;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude={WebMvcAutoConfiguration.class})
+//@EnableAutoConfiguration  //增加exclude 信息，否则不能启动
 @ComponentScan(basePackages = "com.thinkgem.jeesite.cfq.mobile.rest.dao")
 public class EhisApplication {
 
