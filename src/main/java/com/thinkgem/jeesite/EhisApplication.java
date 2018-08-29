bf84e7fa8ed9ce5c3f0d498af374a7e94a5e5d48
bian1234 <ykbian@qq.com>
2018-08-29 15:23:28 +0800

    工程启动
    必须注释掉SicmedWordDao系列文件才能启动  entity dao service controller 以及xml 文件。

--- src/main/java/com/thinkgem/jeesite/EhisApplication.java
+++ src/main/java/com/thinkgem/jeesite/EhisApplication.java
@@ -3,17 +3,18 @@ package com.thinkgem.jeesite;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={WebMvcAutoConfiguration.class})
-//@EnableAutoConfiguration  //增加exclude 信息，否则不能启动
+//@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @ComponentScan(basePackages = "com.thinkgem.jeesite.cfq.mobile.rest.dao")
 public class EhisApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EhisApplication.class, args);
+		System.out.println("=================success================");
 	}
 }
--- src/main/java/com/thinkgem/jeesite/cfq/mobile/rest/bean/SicmedWordBean.java
+++ src/main/java/com/thinkgem/jeesite/cfq/mobile/rest/bean/SicmedWordBean.java
@@ -1,8 +1,8 @@
-package com.thinkgem.jeesite.cfq.mobile.rest.bean;
-
-public class SicmedWordBean extends BaseBean {
-
-    private static final long serialVersionUID = 1L;
-
-
-}
+//package com.thinkgem.jeesite.cfq.mobile.rest.bean;
+//
+//public class SicmedWordBean extends BaseBean {
+//
+//    private static final long serialVersionUID = 1L;
+//
+//
+//}
