9c941d7faa8b8909bf5e99f1365737551b1a4ee5
bian1234 <ykbian@qq.com>
2018-09-07 17:40:14 +0800

    补充科室处方操作。

--- src/main/java/com/sicmed/ehis/controller/SicmedStandardprescriptionBranchController.java
+++ src/main/java/com/sicmed/ehis/controller/SicmedStandardprescriptionBranchController.java
@@ -59,65 +59,14 @@ public class SicmedStandardprescriptionBranchController  extends BaseController 
         }
         return querySuccessResponse(sicmedStandardprescriptionBranches);
     }
-//    /**
-//     * Url:
-//     * http://localhost:8080/jeesite/a/rest/sicmedStandardprescriptionBranch/
-//     * findStandardPrescriptionBranch
-//     *
-//     * @param id(科室id)
-//     *            (21) askedClass(医嘱类别) (检查)
-//     *
-//     * @return 科室标准处方
-//     */
-//    @ResponseBody
-//    @RequestMapping(value = "findStandardPrescriptionBranch")
-//    public SicmedStandardprescriptionBranchBeans findStandardPrescriptionBranch(
-//            @RequestParam(required = false) String id, String askedClass, HttpServletResponse response) {
-//        SicmedStandardprescriptionBranchBeans sicmedStandardprescriptionBranchBeans = new SicmedStandardprescriptionBranchBeans();
-//        try {
-//            //查询科室信息
-//            SicmedBranch sicmedBranch = sicmedBranchService.get(id);
-//            if (sicmedBranch == null) {
-//                sicmedStandardprescriptionBranchBeans.setError("1");
-//                sicmedStandardprescriptionBranchBeans.setErrorMessage("科室编号输入有误或者没有该科室处方");
-//            } else {
-//                //查询医嘱类别信息
-//                SicmedAskedClass sicmedAskedClass = sicmedAskedClassService.getAskedByAskedClass(askedClass);
-//                SicmedStandardprescriptionBranch sicmedStandardprescriptionBranch = new SicmedStandardprescriptionBranch();
-//                sicmedStandardprescriptionBranch.setSicmedBranch(sicmedBranch);
-//                sicmedStandardprescriptionBranch.setAskedClass(sicmedAskedClass);
-//
-//                //查询到标准处方列表
-//                List<SicmedStandardprescriptionBranch> sicmedStandardprescriptionBranchs = sicmedStandardprescriptionBranchService
-//                        .findList(sicmedStandardprescriptionBranch);
-//
-//                if (sicmedStandardprescriptionBranchs.size() != 0) {
-//                    for (SicmedStandardprescriptionBranch sicmedStandardprescriptionBranch2 : sicmedStandardprescriptionBranchs) {
-//                        SicmedStandardprescriptionBranchBean sicmedStandardprescriptionBranchBean = new SicmedStandardprescriptionBranchBean();
-//                        // sicmedStandardprescriptionBranchBeans.getSicmedStandardprescriptionBranchBeans().add(sicmedStandardprescriptionBranchBean);
-//                        sicmedStandardprescriptionBranch2.setSicmedBranch(sicmedBranch);
-//                        sicmedStandardprescriptionBranch2.setAskedClass(sicmedAskedClass);
-//                        // sicmedStandardprescriptionBranch2.setStanderprescriptionDisease(sicmedStanderprescriptionDiseaseService.get(sicmedStandardprescriptionBranch2.getStanderprescriptionDisease().getId()));
-//                        // sicmedStandardprescriptionBranch2.setStanderprescriptionAsked(sicmedStanderprescriptionAskedService.get(sicmedStandardprescriptionBranch2.getStanderprescriptionAsked().getId()));
-//                        sicmedStandardprescriptionBranchBean.setssb(sicmedStandardprescriptionBranch2);
-//                        sicmedStandardprescriptionBranchBeans.getSicmedStandardprescriptionBranchBeans()
-//                                .add(sicmedStandardprescriptionBranchBean);
-//                    }
-//                    sicmedStandardprescriptionBranchBeans.setError("0");
-//                    sicmedStandardprescriptionBranchBeans.setErrorMessage("科室标准处方查看成功");
-//                } else {
-//                    sicmedStandardprescriptionBranchBeans.setError("1");
-//                    sicmedStandardprescriptionBranchBeans.setErrorMessage("查询失败,找不到信息");
-//                }
-//            }
-//
-//        } catch (Exception e) {
-//            sicmedStandardprescriptionBranchBeans.setError("1");
-//            sicmedStandardprescriptionBranchBeans.setErrorMessage("查询失败!系统内部异常" + e.getMessage());
-//        }
-//        return sicmedStandardprescriptionBranchBeans;
-//    }
 
+
+    /**
+     *@Author:      ykbian
+     *@date_time:   2018/9/7 17:39
+     *@Description:   通过id查询
+     *@param:
+    */
     @GetMapping("selectById")
     @ResponseBody
     public Map selectById(String id) {
